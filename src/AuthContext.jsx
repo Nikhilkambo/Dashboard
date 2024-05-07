e50dd894537985763c6ff387db3f5e32a85d4d27
“Nikhilkambo” <kambojnikhil307@gmail.com>
2024-05-07 17:23:27 +0530

    changes

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,12 +1,14 @@
 import React, { createContext, useState, useEffect } from "react";
 const AuthContext = createContext();
 
-
 const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [productList, setProductList] = useState([]);
   const [modalShow, setModalShow] = useState(false);
+  const [updatePage, setUpdatePage] = useState(true);
+  const [updateProduct, setUpdateProduct] = useState([]);
+  const [updateModel, setUpdateModel] = useState(false);
 
   useEffect(() => {
     // Check token expiry on app initialization
@@ -14,15 +16,15 @@ const AuthProvider = ({ children }) => {
   }, []);
 
   const checkTokenExpiry = () => {
-    const storedToken = localStorage.getItem('token');
-    console.log(storedToken,"this is stored ")
+    const storedToken = localStorage.getItem("token");
+    console.log(storedToken, "this is stored ");
     if (storedToken) {
       // Here you would normally check if the token is expired
       // For simplicity, we'll just assume the token is valid for 30 seconds
       // const decodedToken = jwt_decode(storedToken);
       // const expiry = decodedToken.exp * 1000;
       // const isTokenValid = expiry > Date.now();
-     const isTokenValid =true
+      const isTokenValid = true;
       if (!isTokenValid) {
         // Token expired, log out
         logout();
@@ -46,12 +48,30 @@ const AuthProvider = ({ children }) => {
     setIsAuthenticated(false);
     localStorage.clear();
   };
-  const productDetails=(productlist)=>{
-    setProductList(productlist)
-  }
-  
+  const productDetails = (productlist) => {
+    setProductList(productlist);
+  };
+  // Handle Page Update
+  const handlePageUpdate = () => {
+    setUpdatePage(!updatePage);
+  };
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout,productDetails,productList,modalShow,setModalShow }}>
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        login,
+        logout,
+        productDetails,
+        productList,
+        modalShow,
+        setModalShow,
+        handlePageUpdate,
+        updatePage,
+        updateProduct,
+        setUpdateProduct,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
